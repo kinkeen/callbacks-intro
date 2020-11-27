edb2b8bcfe0209878cc44c1cbaa4ec6a115fd8a1
kineret.braslavsky <kineret.braslavsky@gmail.com>
2020-11-27 05:23:08 +0000

    implement findWaldo.js, findWaldoAnonymously.js

---
+++ findWaldoAnonymously.js
@@ -0,0 +1,13 @@
+// The second argument/parameter is expected to be a (callback) function
+const findWaldo = function(names, found) {
+    for (let i = 0; i < names.length; i++) {
+      let name = names[i];
+      if (name === "Waldo") {
+        found(i);   // execute callback
+      }
+    }
+  }
+ 
+  findWaldo(["Alice", "Bob", "Waldo", "Winston"], function(index) {
+    console.log("Waldo is located at:", index);
+  });
