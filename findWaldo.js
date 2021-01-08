bb04667f920fb013d1acf5fd705442632ffede3d
kineret.braslavsky <kineret.braslavsky@gmail.com>
2021-01-08 01:53:35 +0000

    Refactor the function findWaldo to use the forEach() method instead of a for loop

--- findWaldo.js
+++ findWaldo.js
@@ -1,11 +1,19 @@
 // The second argument/parameter is expected to be a (callback) function
-const findWaldo = function(names, found) {
+/*const findWaldo = function(names, found) {
   for (let i = 0; i < names.length; i++) {
     let name = names[i];
     if (name === "Waldo") {
       found(i);   // execute callback
     }
   }
+}*/
+
+const findWaldo = function(names, found) {
+  names.forEach(name => {
+    if (name === "Waldo") {
+      found();   // execute callback
+    }
+  });
 }
 
 const actionWhenFound = function(index) {
